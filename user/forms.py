1b5a4771d075eda7da29e9969f6964b056534773
Junghyun <bongtacker@naver.com>
2023-10-31 08:24:40 +0900

    change html

--- user/forms.py
+++ user/forms.py
@@ -5,8 +5,8 @@ from .models import UserBodyInput
 class UserBodyInputForm(forms.ModelForm):
     class Meta:
         model = UserBodyInput
-        fields = ['gender', 'height', 'weight']
+        fields = ['height', 'weight', 'gender']
 
-    gender = forms.CharField()
     height = forms.FloatField(min_value=0)
     weight = forms.FloatField(min_value=0)
+    gender = forms.CharField()

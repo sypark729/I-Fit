cad88e271ee7e9c7fec96c7fd0afab78f1f11933
RandomMap <goldspdlv12@naver.com>
2023-11-12 22:04:58 +0900

    fix web design

--- sizing/settings.py
+++ sizing/settings.py
@@ -134,3 +134,6 @@ STATICFILES_DIRS = ( os.path.join("static"),)
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
